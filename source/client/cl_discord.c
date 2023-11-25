d69321736d2c084f2ac4d1eff3157c49222e9140
Warfork <51063252+Warfork@users.noreply.github.com>
2023-11-25 18:06:01 -0500

    Discord -> Invite to join\connecting to loopback shouldn't be permitted
    Fixed by learn_more: https://github.com/TeamForbiddenLLC/warfork-qfusion/issues/238

--- source/client/cl_discord.c
+++ source/client/cl_discord.c
@@ -617,7 +617,7 @@ void CL_UpdateDiscord( void )
 				strcpy( presence.partyId, cl.configstrings[CS_HOSTNAME] );
 
 				// If server is not localhost
-				if( cls.servertype != SOCKET_LOOPBACK ) {
+				if( cls.servertype != NET_IsLocalAddress ) {
 					strcpy( presence.joinSecret, NET_AddressToString( &cls.serveraddress ) );
 				}
 
