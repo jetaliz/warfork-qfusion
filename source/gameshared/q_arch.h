5c2e305a3d9fffbdeb72b3a36f32d4591f55bd31
Warfork <51063252+Warfork@users.noreply.github.com>
2023-11-30 17:24:13 -0500

    Update q_arch.h

--- source/gameshared/q_arch.h
+++ source/gameshared/q_arch.h
@@ -127,10 +127,7 @@ extern "C" {
 #define OPENAL_RUNTIME
 
 // FIXME: move these to CMakeLists.txt
-#define LIBZ_LIBNAME "zlib1.dll"
 #define LIBCURL_LIBNAME "libcurl-4.dll|libcurl-3.dll"
-#define LIBPNG_LIBNAME "libpng16.dll|libpng16-16.dll|libpng15-15.dll|libpng15.dll|libpng14-14.dll|libpng14.dll|libpng12.dll"
-#define LIBJPEG_LIBNAME "libjpeg.dll"
 #define LIBOGG_LIBNAME "libogg-0.dll|libogg.dll"
 #define LIBVORBIS_LIBNAME "libvorbis-0.dll|libvorbis.dll|vorbis.dll"
 #define LIBVORBISFILE_LIBNAME "libvorbisfile-3.dll|libvorbisfile.dll|vorbisfile.dll"
@@ -202,10 +199,7 @@ typedef UINT_PTR socket_handle_t;
 #define OPENAL_RUNTIME
 
 // FIXME: move these to CMakeLists.txt
-#define LIBZ_LIBNAME "libz.so.1|libz.so"
 #define LIBCURL_LIBNAME "libcurl.so.4|libcurl.so.3|libcurl.so"
-#define LIBPNG_LIBNAME "libpng16.so.16|libpng15.so.15|libpng14.so.14|libpng12.so.0|libpng.so"
-#define LIBJPEG_LIBNAME "libjpeg.so.8|libjpeg.so"
 #define LIBOGG_LIBNAME "libogg.so.0|libogg.so"
 #define LIBVORBIS_LIBNAME "libvorbis.so.0|libvorbis.so"
 #define LIBVORBISFILE_LIBNAME "libvorbisfile.so.3|libvorbisfile.so"
@@ -300,10 +294,7 @@ typedef int socket_handle_t;
 #define OPENAL_RUNTIME
 
 // FIXME: move these to CMakeLists.txt
-#define LIBZ_LIBNAME "libz.dylib"
 #define LIBCURL_LIBNAME "libcurl.4.dylib|libcurl.3.dylib|libcurl.2.dylib"
-#define LIBPNG_LIBNAME "libpng16.16.dylib|libpng15.15.dylib|libpng14.14.dylib|libpng12.0.dylib"
-#define LIBJPEG_LIBNAME "libjpeg.62.dylib"
 #define LIBOGG_LIBNAME "libogg.0.dylib|libogg.dylib"
 #define LIBVORBIS_LIBNAME "libvorbis.dylib"
 #define LIBVORBISFILE_LIBNAME "libvorbisfile.dylib"
