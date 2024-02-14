9410daa64601d85849034ce0bcf241c7e460beac
Michael Pollind <mpollind@gmail.com>
2024-02-13 17:46:18 -0800

    feat: add components for unit testing
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

---
+++ source/ref_base/test/r_texture_buffer_test.c
@@ -0,0 +1,25 @@
+#include "unity.h"
+#include "../ref_base/r_texture_buf.h"
+
+void setUp(void)
+{
+}
+
+void tearDown()
+{
+}
+void test_FindFunction_WhichIsBroken_ShouldReturnZeroIfItemIsNotInList_WhichWorksEvenInOurBrokenCode(void)
+{
+  /* All of these should pass */
+//  TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(78));
+//  TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(2));
+//  TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(33));
+//  TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(999));
+//  TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(-1));
+}
+
+int main(void)
+{
+UNITY_BEGIN();
+return UNITY_END();
+}
